7488a7fc71c005da8293cedea502acdecde96115
caichen <icaichen@yahoo.com>
2025-12-03 23:14:14 +0800

    play store api update

--- lib/config/revenuecat_config.dart
+++ lib/config/revenuecat_config.dart
@@ -1,8 +1,7 @@
 class RevenueCatConfig {
   // RevenueCat API keys for KeepJoy
-  // Note: Using the same test key for both iOS and Android
   static const String iosApiKey = 'appl_yYSRJoqXWidFWjLLwAFrGhVnCWM';
-  static const String androidApiKey = 'test_vpvyoUzQEgHvDNRCxDjxmlPKhYW';
+  static const String androidApiKey = 'goog_avJhUHmwvDEBgpaTqgSxVLPrchk';
 
   static const String defaultOfferingId = 'default';
   static const String premiumEntitlementId = 'premium';
